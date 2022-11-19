6b444cbebb95dcb7d33fd7edd3e970f2dea09fb2
Muxtor <jalilovmuxtor203@gmail.com>
2022-11-19 23:21:57 +0500

    20

--- logic20.py
+++ logic20.py
@@ -1,4 +1,4 @@
-def main(n):
+def main(a):
     """
     A number consisting of one and zero is given (the number starts at once). 
     If the number of ones is greater than zero, true, otherwise False is returned.
@@ -8,5 +8,16 @@ def main(n):
     Returns:
         bool: answer
     """
+    x1=a%10
+    a=a//10
+    x2=a%10
+    a=a//10
+    x3=a%10
+    a=a//10
+    x4=a%10
+    a=a//10
+    x5=a%10
+    a=a//10
 
-    return
+    return (x1+x2+x3+x4+x5)>2
+print(main(11001))
